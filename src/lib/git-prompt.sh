d1b2b9f0011b741dfa3f85d99f08a954d8156e82
Ben Turner <bbgrabbag@gmail.com>
2025-12-11 19:44:19 -0700

    feat(!): refactor and rebrand as ebt-toolkit
    * feat(!): Enhance Dockerfile and add CLI scripts for git management. Install bash-completion, copy CLI scripts, and configure git prompt in bash. Update devcontainer.json for EBT CLI and add interactive scripts for commit and branch management.
    
    * docs: update readme to contain registry domain (#1)
    
    * chore(ci): create github action workflow for tagging abd building docker image
    
    * fix: Fix typo in workflow
    
    * fix: fix typo in regex
    
    * fix: another attempt
    
    * fix: add ticket to dev tag
    
    * chore(ci): replace local build workflow with organization repo referene
    
    * chore: update path to resuable workflow
    
    * chore: remove local actions, fix workflow refernce link
    
    * docs: add docs to readme
    
    * fix: troubleshooting workflow reference
    
    * fix: revert changes after making repo public
    
    * docs: update readme to contain registry domain
    
    ---------
    
    Co-authored-by: Ben Turner <ben.turner@territories.ai>
    
    * feat!:enable github actions
    
    * chore(ci): create github action workflow for tagging abd building docker image
    
    * fix: Fix typo in workflow
    
    * fix: fix typo in regex
    
    * fix: another attempt
    
    * fix: add ticket to dev tag
    
    * chore(ci): replace local build workflow with organization repo referene
    
    * chore: update path to resuable workflow
    
    * chore: remove local actions, fix workflow refernce link
    
    * docs: add docs to readme
    
    * fix: troubleshooting workflow reference
    
    * fix: revert changes after making repo public
    
    * docs: update readme to contain registry domain
    
    * chore(auth): fix auth permission
    
    ---------
    
    Co-authored-by: Ben Turner <ben.turner@territories.ai>
    
    * feat!: add entrpoiny
    
    * chore(ci): create github action workflow for tagging abd building docker image
    
    * fix: Fix typo in workflow
    
    * fix: fix typo in regex
    
    * fix: another attempt
    
    * fix: add ticket to dev tag
    
    * chore(ci): replace local build workflow with organization repo referene
    
    * chore: update path to resuable workflow
    
    * chore: remove local actions, fix workflow refernce link
    
    * docs: add docs to readme
    
    * fix: troubleshooting workflow reference
    
    * fix: revert changes after making repo public
    
    * docs: update readme to contain registry domain
    
    * chore(auth): fix auth permission
    
    * feat: move run commands to config script
    
    * chore(!): move config/sh to entrypoint
    
    ---------
    
    Co-authored-by: Ben Turner <ben.turner@territories.ai>
    
    * refactor: split out git completion and installation
    
    * refactor: split out git completion and installation
    
    * fix: add missing shebang
    
    ---------
    
    Co-authored-by: Ben Turner <ben.turner@territories.ai>
    
    * refactor: 
    
    * feat(vendor): install aws and gh cli
    
    * fix(ci): safely install gh cli
    
    * refactor(vendor): move vendor isntallation out of cli lib
    
    * docs: update docs and rename to toolkit
    
    ---------
    
    Co-authored-by: Ben Turner <ben.turner@territories.ai>
    
    ---------
    
    Co-authored-by: Ben Turner <ben.turner@territories.ai>

--- src/lib/git-prompt.sh
+++ src/lib/git-prompt.sh
@@ -8,5 +8,3 @@ parse_git_branch() {
 # Custom prompt with git branch
 # Format: user@host:directory (branch) $
 export PS1='\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\[\033[01;33m\]$(parse_git_branch)\[\033[00m\]\$ '
-
-
